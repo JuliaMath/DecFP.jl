54341669058d920d40ffbfcb7b2886312b9fae0b
Steven G. Johnson <stevenj@mit.edu>
2015-04-28 22:02:04 -0400

    needed to disable thread-local storage on Windows, since it generates emutls globals that I can't access from Julia

--- deps/build.jl
+++ deps/build.jl
@@ -8,8 +8,8 @@ target = "libbid$WORD_SIZE.$(Libdl.dlext)"
 if !isfile(tagfile) || !isfile(target) || readchomp(tagfile) != "$vers $WORD_SIZE"
     if OS_NAME == :Windows
         # binary for Windows was cross-compiled with mingw using:
-        # 32-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86 _NUM_CPUS=1 CC=i686-w64-mingw32-gcc CFLAGS_OPT=-O2 CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-        # 64-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86_64 _NUM_CPUS=1 CC=x86_64-w64-mingw32-gcc CFLAGS_OPT=-O2 CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+        # 32-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86 _NUM_CPUS=1 CC=i686-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD=" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+        # 64-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86_64 _NUM_CPUS=1 CC=x86_64-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD=" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
         run(download_cmd("http://ab-initio.mit.edu/decfp/libbid$WORD_SIZE-$vers.dll", target))
     elseif OS_NAME == :Darwin
         run(download_cmd("http://ab-initio.mit.edu/decfp/libbid$WORD_SIZE-$vers.dylib", target))
