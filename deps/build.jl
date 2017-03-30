af7c32309ec23cd81038247e41e34c018e4dfd15
Steven G. Johnson <stevenj@alum.mit.edu>
2017-03-29 21:53:11 -0400

    note cross-compile/mingw fix for windows from #28

--- deps/build.jl
+++ deps/build.jl
@@ -8,8 +8,12 @@ target = "libbid$(Sys.WORD_SIZE).$(Libdl.dlext)"
 if !isfile(tagfile) || !isfile(target) || readchomp(tagfile) != "$vers $(Sys.WORD_SIZE)"
     if Compat.KERNEL == :NT
         # binary for Windows was cross-compiled with mingw using:
-        # 32-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86 _NUM_CPUS=1 CC=i686-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD=" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-        # 64-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86_64 _NUM_CPUS=1 CC=x86_64-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD=" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+        # 32-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86 _NUM_CPUS=1 CC=i686-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD= -DBID_MS_FLAGS" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+        # 64-bit: CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86_64 _NUM_CPUS=1 CC=x86_64-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD= -DBID_MS_FLAGS" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+        # with the makefile.shared file:
+        #   include makefile
+        #   libbid.dll: $(ALL_BID_OBJS)
+        #           $(CC) -shared -o $@ $(ALL_BID_OBJS)
         run(download_cmd("http://ab-initio.mit.edu/decfp/libbid$(Sys.WORD_SIZE)-$vers.dll", target))
     elseif Compat.KERNEL == :Darwin
         run(download_cmd("http://ab-initio.mit.edu/decfp/libbid$(Sys.WORD_SIZE)-$vers.dylib", target))
