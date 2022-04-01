ba81440c80bcd0ced92dc7c22e13a3995aa0f984
Dan Taras <dan.taras@graystonetrading.com>
2022-04-01 10:38:09 +0200

    read and write methods. Fixes #160

--- src/DecFP.jl
+++ src/DecFP.jl
@@ -140,6 +140,8 @@ struct Dec128 <: DecimalFloatingPoint
 end
 Base.reinterpret(::Type{UInt128}, x::Dec128) = x.x
 Base.bswap(x::Dec128) = reinterpret(Dec128, bswap(x.x))
+Base.write(s::IO, x::Union{Dec32, Dec64, Dec128}) = write(s, Ref(x))
+Base.read(s::IO, T::Union{Type{Dec32}, Type{Dec64}, Type{Dec128}}) = read!(s, Ref{T}(zero(T)))[]::T
 
 for w in (32,64,128)
     BID = Symbol(string("Dec",w))
